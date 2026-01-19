fea1491935b33eff066a83b1e6717f76113d436e
Marc Huwiler <marc.huwiler@cern.ch>
2026-01-19 12:23:16 +0100

    Adding tagger vars for third fat jet

--- Reco.py
+++ Reco.py
@@ -197,6 +197,10 @@ if __name__ == "__main__":
 	gensig = DefineBTaggerVarsForJet("GenMatchedbFatJet", gensig)
 	gensig = DefineTauTaggerVarsForJet("GenMatchedbFatJet", gensig)
 	gensig = DefineBTaggerVarsForJet("GenMatchedTauFatJet", gensig)
+	gensig = gensig.Define("SubSubLeadingFatJet", "3")
+	gensig = DefineTauTaggerVarsForJet("SubSubLeadingFatJet", gensig)
+	gensig = DefineBTaggerVarsForJet("SubSubLeadingFatJet", gensig)
+
 
 
 	gensig = gensig.Define("dR_gen_reco_mu", "Ana::deltaR(GenDecay.mu, {0}_pt, {0}_eta, {0}_phi, {0}_mass, {1}_pt, {1}_eta, {1}_phi, {1}_mass, TheMuon)".format("GenPart", "Muon"))
