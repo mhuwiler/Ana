025df39703088d2dc59229cb16d60839feaecec1
Marc Huwiler <marc.huwilercern.ch>
2026-04-21 11:57:51 +0200

    Applying prescale weight only to scouting

--- analysis/event_loop.py
+++ analysis/event_loop.py
@@ -130,11 +130,15 @@ def book_and_run(
                 data_sel = data_sel.Filter(cut_expr)
             mc_sel = {name: df.Filter(cut_expr) for name, df in mc_sel.items()}
 
+        weighting = "w"
+        if (trig_name == "DST_JetHT"): weighting = "finalWeight"
+        print(trig_name)
+
         sig_mHH_ptrs = [
             mc_sel[s].Histo1D(
                 (f"h_mHH_{s}_{trig_name}",
                  "m_{HH} gen-level;m_{HH} [GeV];Events", 24, 0, 1200),
-                "gen_mHH", "finalWeight")
+                "gen_mHH", weighting)
             for s in sig_samples
         ]
         unified_ptrs.extend(sig_mHH_ptrs)
@@ -174,10 +178,10 @@ def book_and_run(
         if trig is None:
             continue
         ptrs = [
-            mc_base[s].Filter(trig).Define("prescaleWeight", "prescaleWeight(L1_HTT200er, L1_HTT255er, L1_HTT280er, L1_HTT320er, L1_HTT360er, L1_HTT400er, L1_HTT450er, L1_ETT2000, L1_SingleJet180, L1_SingleJet200, L1_DoubleJet30er2p5_Mass_Min250_dEta_Max1p5, L1_DoubleJet30er2p5_Mass_Min300_dEta_Max1p5, L1_DoubleJet30er2p5_Mass_Min330_dEta_Max1p5)").Define("finalWeight", "w*prescaleWeight").Histo1D(
+            mc_base[s].Filter(trig).Histo1D(
                 (f"h_mHH_{s}_{trig_name}",
                  "m_{HH} gen-level;m_{HH} [GeV];Events", 24, 0, 1200),
-                "gen_mHH", "finalWeight")
+                "gen_mHH", "w")
             for s in sig_samples
         ]
         unified_ptrs.extend(ptrs)
